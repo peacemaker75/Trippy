c90ef2db6500adadad3775194248ae4ccb109a53
bilel ayachi <daskik75@gmail.com>
2020-08-12 18:06:02 +0200

    update 3

--- src/components/core/Home.js
+++ src/components/core/Home.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 
 class Home extends React.Component {
+    
+    
 
     render() {
         return(
--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
 class HomeContainer extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      cities: []
+    }
+  }
+  
+  componentDidMount = () => {
+    fetch('http://localhost:3001/api/home')
+    .then(res => res.json())
+    .then(json => {
+      console.log(json)
+      this.setState({
+        cities: json.cities
+      })
+  });
+  }
+
   render() {
+    console.log(this.state.cities)
     return (
       <div
         className='container-fluid'
@@ -10,16 +30,16 @@ class HomeContainer extends React.Component {
         }}>
         <div
           className='row'>
-          <div
-            className={'col-lg-4 col-md-8 col-12'}>
-            <p>
-              Je vérifie que les 2 mots : Voleur et <span style={{fontFamily: 'serif' }}>Voleur</span> ont une police d'écriture différente
-            </p>
-          </div>
-          <div
-            className='col-lg-8 col-md-4 col-12'>
-            Une autre phrase
-          </div>
+            {this.state.cities.map((result) => {
+              if (result.name  === 'Paris'){
+              return (
+            <div>{result.name} + est magique</div>
+              ) } 
+              else {
+                return(
+                <div>{result.name} + superville</div>
+              )};
+  })}
         </div>
       </div>
     );
